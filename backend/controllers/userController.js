2e84213ff367feb5b3b85c3862c93b125c4e0618
Mukesh-KB1 <evermukesh1@gmail.com>
2026-01-17 01:46:24 +0530

    ignore .env & update userRoute

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,7 @@ const loginUser = async(req,res)=>{
 
 //Route for user Register
 const registerUser = async(req,res)=>{
-
+    res.json({msg: "register API is working.."})
 }
 
 //Route for Admin Login
