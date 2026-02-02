9ed6e080d00a67cc401055850cf64d39e8455a83
Mukesh-KB1 <evermukesh1@gmail.com>
2026-02-03 01:25:47 +0530

    AdminLink added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -60,6 +60,15 @@ function Navbar() {
                     <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
                 </NavLink>
 
+                {/* <a
+                    href="https://zenvee-admin.vercel.app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="ml-2 px-4 py-1.5 border border-gray-300 rounded-full text-xs text-gray-700 hover:bg-gray-100 transition"
+                >
+                    Admin Panel
+                </a> */}
+
             </ul>
 
             <div className='flex items-center gap-6'>
@@ -72,8 +81,15 @@ function Navbar() {
                     {token &&
                         <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
                             <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
-                                <p className='cursor-pointer hover:text-black'>My Profile</p>
-                                <p onClick={()=> navigate('/Orders')} className='cursor-pointer hover:text-black'>Orders</p>
+                                <a
+                                    href="https://zenvee-admin.vercel.app/"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="cursor-pointer hover:text-black"
+                                >
+                                    Admin Panel
+                                </a>
+                                <p onClick={() => navigate('/Orders')} className='cursor-pointer hover:text-black'>Orders</p>
                                 <p onClick={() => logOutUser()} className='cursor-pointer hover:text-black'>Logout</p>
                             </div>
                         </div>
